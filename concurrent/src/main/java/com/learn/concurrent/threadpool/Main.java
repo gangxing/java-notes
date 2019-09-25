c79233f7ff69fe3915dbe02fb37c1386cde9c70d
xianjun <xianjun@ixiaopu.com>
2019-09-25 10:13:32 +0800

    编译通过

--- concurrent/src/main/java/com/learn/concurrent/threadpool/Main.java
+++ concurrent/src/main/java/com/learn/concurrent/threadpool/Main.java
@@ -10,5 +10,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        for (int c=0;c<127;c++){
+//            System.err.println(c+"="+(char)c);
+//        }
+        long max=1L<<62;
+        System.err.println(max);
+
+        long rate=1024 * 1024 *1024;
+
+        System.err.println(max/rate);
     }
 }
--- datastructure/src/main/java/java/lang/String.java
+++
@@ -1,9 +0,0 @@
-package java.lang;
-
-/**
- * @Description
- * @Author xgangzai
- * @Date 2019/9/6 10:20
- */
-public class String {
-}
