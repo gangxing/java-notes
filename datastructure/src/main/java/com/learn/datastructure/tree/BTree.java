770c4167a337438af682c2a643daf2da883a0118
xgang <xgangzai@gmail.com>
2019-10-17 15:54:36 +0800

    B树

--- datastructure/src/main/java/com/learn/datastructure/tree/BTree.java
+++ datastructure/src/main/java/com/learn/datastructure/tree/BTree.java
@@ -7,4 +7,58 @@ package com.learn.datastructure.tree;
  * @Date 2019/7/16 09:32
  */
 public class BTree {
+
+    /**
+     * 阶数
+     */
+    private int m;
+
+    /**
+     * 根节点
+     */
+    private Node root;
+
+    public BTree(int m) {
+        this.m = m;
+    }
+
+    //元素，key - value
+    private class Item {
+        int key;
+
+        String value;
+
+        public Item(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    // 节点
+    private class Node {
+        Item[] items;
+
+        int count;
+
+        Node parent;
+
+        Node left;
+
+        Node right;
+
+        public Node(Item item) {
+            items = new Item[m];
+            items[0] = item;
+            count = 1;
+        }
+
+        public void add(Item item) {
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
---
+++ datastructure/src/main/java/com/learn/datastructure/tree/BTreeTest.java
@@ -0,0 +1,15 @@
+package com.learn.datastructure.tree;
+
+/**
+ * @Description
+ * @Author xgangzai
+ * @Date 2019/10/17 15:51
+ */
+public class BTreeTest {
+
+    public static void main(String[] args) {
+        int m=5;
+        BTree bTree=new BTree(m);
+
+    }
+}
