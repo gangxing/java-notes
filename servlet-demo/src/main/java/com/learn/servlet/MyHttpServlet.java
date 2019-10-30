5de29e04191d40873d3e043e18cd0634a37bb205
xgang <xgangzai@gmail.com>
2019-10-30 11:23:34 +0800

    servlet-api学习

---
+++ servlet-demo/src/main/java/com/learn/servlet/MyHttpServlet.java
@@ -0,0 +1,21 @@
+package com.learn.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description
+ * @Author xgangzai
+ * @Date 2019/10/30 11:13
+ */
+public class MyHttpServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
