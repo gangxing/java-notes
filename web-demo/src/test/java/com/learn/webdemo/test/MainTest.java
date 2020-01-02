4a8edeb83b25936f6186f176ee417c7596272fcf
mockuai <mockuai@mockuaideMacBook-Pro.local>
2020-01-02 15:45:12 +0800

    新的开发环境

---
+++ web-demo/src/test/java/com/learn/webdemo/test/MainTest.java
@@ -0,0 +1,13 @@
+package com.learn.webdemo.test;
+
+import com.learn.webdemo.model.entity.UserEntity;
+
+public class MainTest {
+
+
+    public static void main(String[] args) {
+        UserEntity user=new UserEntity();
+        user.setName("xxx");
+        System.err.println(user.toString());
+    }
+}
